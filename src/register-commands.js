ea44dfd7c5f73a3298fa4a8fb59c8c02dd475544
Ranoa <aaron.cabling@etu.unice.fr>
2024-04-24 19:48:11 +0200

    code comment

--- src/register-commands.js
+++ src/register-commands.js
@@ -1,6 +1,10 @@
 require('dotenv').config();
 const {REST, Routes} = require('discord.js');
 
+/**
+ * @const commands
+ * Array of available slash commands
+ */
 const commands = [
   {
     name: 'hello',
@@ -10,11 +14,16 @@ const commands = [
 
 const rest = new REST({version: '10'}).setToken(process.env.TOKEN);
 
+/**
+ * @function
+ * Register slash commands via CLIENT_ID and GUILD_ID based on commands
+ */
 (async () => {
   try {
     console.log('Registering slash commands...');
 
     await rest.put(
+      // Define the route for slash commands
       Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
       {body: commands}
     );
